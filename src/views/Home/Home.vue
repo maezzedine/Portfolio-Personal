159669920ddc354db41891a11aa2be40e0547435
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2021-05-06 01:06:49 +0300

    Finish the home page

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,7 @@
     <div class="name">Mohammed Ezzedine</div>
     <div class="occupation">Computer Science Student</div>
     <div class="field">Software Engineering and Machine Learning</div>
+    <a class="arrow" href="#overview"><v-icon>mdi-chevron-down</v-icon></a>
   </div>
 </template>
 
--- src/views/Overview/Overview.vue
+++ src/views/Overview/Overview.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="overview">
+  <div class="overview" id="overview">
     <!-- <router-view/> -->
-    x
+    <v-app-bar
+      color="#18212b"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <!-- <v-toolbar-title>Collapsing Bar</v-toolbar-title> -->
+
+      <v-spacer></v-spacer>
+
+      <v-tabs align-with-title>
+        <v-tab>Tab 1</v-tab>
+        <v-tab>Tab 2</v-tab>
+        <v-tab>Tab 3</v-tab>
+      </v-tabs>
+    </v-app-bar>
+    
+    <!-- <v-sheet
+      id="scrolling-techniques-6"
+      class="overflow-y-auto"
+      max-height="100vh"
+    >
+        <div style="height: 120vh"></div>
+    </v-sheet> -->
+
   </div>
 </template>
 
